721da5a7731dbef035be25389ce2bc66e3473b7b
Sumnoon Ibn Ahmad <sumnoon52@gmail.com>
2018-11-12 19:53:15 +0600

    Update 1275.cpp

--- Basic Math/1275.cpp
+++ Basic Math/1275.cpp
@@ -3,27 +3,21 @@
 using namespace std;
 typedef long long ll;
 int main() {
-	int tc, cases = 0;
-	ll n, c, ans;
+	int tc, cases = 0, n, c, ans;
 	scanf("%d", &tc);
 	while ( tc-- ) {
-		scanf("%lld %lld", &n, &c);
+		scanf("%d %d", &n, &c);
 		if ( !n || !c ) ans = 0;
 		else if ( n < c && 2 * n > c ) ans = 1;
 		else if ( c % (2*n) == 0 ) ans = c/(2*n);
 		else {
 			ll t = c / (2*n);
 			ll mx = t * ( c -  t * n );
+			ll mx1 = (t+1)*(c - (t+1)*n);
 			ans = t;
-			while ( c > ( t * n ) ) {
-				if ( mx < t * ( c - n * t ) ) {
-					mx = max ( mx, t * ( c - n * t ) );
-					ans ++;
-				}
-				t++;
-			}
+			if ( mx1 > 0 && mx1 > mx ) ans++;
 		}
-		printf("Case %d: %lld\n", ++cases, ans);
+		printf("Case %d: %d\n", ++cases, ans);
 	}
 	return 0;
 }
